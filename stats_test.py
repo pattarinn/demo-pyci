28fbdc0aaa8735854a7a5ed03c5493feef431fa5
Pattarin <pattarin.won@ku.th>
2020-10-05 22:08:45 +0700

    add test for stdev

--- stats_test.py
+++ stats_test.py
@@ -1,14 +1,14 @@
 from unittest import TestCase
-from stats import variance
+from stats import variance, stdev
 
 
 class StatsTest(TestCase):
 
     def test_variance_typical_values(self):
         """variance of some typical values"""
-        self.assertEqual(0.0, variance([10.0,10.0,10.0,10.0,10.0]))
-        self.assertEqual(2.0, variance([1,2,3,4,5]))
-        self.assertEqual(8.0, variance([10,2,8,4,6]))
+        self.assertEqual(0.0, variance([10.0, 10.0, 10.0, 10.0, 10.0]))
+        self.assertEqual(2.0, variance([1, 2, 3, 4, 5]))
+        self.assertEqual(8.0, variance([10, 2, 8, 4, 6]))
 
     def test_variance_single_value_is_zero(self):
         """variance of a singleton list should be zero"""
@@ -20,7 +20,12 @@ class StatsTest(TestCase):
         with self.assertRaises(ValueError):
             var = variance([])
 
+    def test_stdev(self):
+        """standard deviation is sqrt of variance"""
+        self.assertEqual(0.0, stdev([2, 2, 2, 2]))
+
+
 if __name__ == '__main__':
     import unittest
-    unittest.main(verbosity=1)
 
+    unittest.main(verbosity=1)
